ba43f7405f75847ab8386a932626bc18f97cbc38
Katerina881 <mr.alodov@mail.ru>
2020-04-30 12:10:01 +0400

    1

---
+++ KorytoModelT/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace KorytoModel
+{
+    [DataContract]
+    public class Car
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        [Required]
+        public string CarName { get; set; }
+
+        [Required]
+        [DataMember]
+        public decimal Price { get; set; }
+
+        [Required]
+        [DataMember]
+        public int Year { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<CarDetail> CarDetails { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual List<OrderCar> OrderCars { get; set; }
+    }
+}
