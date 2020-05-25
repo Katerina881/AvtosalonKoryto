bfd5d4ce11ee6540f2f5c65949fc7e0100592aef
Ринат Хисамов <khisamov99@gmail.com>
2020-05-25 20:10:00 +0400

    25.05

---
+++ KorytoDatabase/KorytoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using KorytoModel;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace KorytoDatabase
+{
+    public class KorytoDbContext : DbContext
+    {
+        public KorytoDbContext() : base("KorytoDatabase")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            
+        }
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Detail> Details { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<CarDetail> CarDetails { get; set; }
+        public virtual DbSet<OrderCar> OrderCars { get; set; }
+        public virtual DbSet<Request> Requests { get; set; }
+        public virtual DbSet<DetailRequest> DetailRequests { get; set; }
+    }
+}
