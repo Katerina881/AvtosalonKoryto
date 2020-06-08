d16c9172bb392494963b6c403a8b648a6cb4cd8c
Katerina881 <mr.alodov@mail.ru>
2020-06-08 13:46:14 +0400

    08

---
+++ KorytoView/FormCar.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace KorytoView
+{
+    partial class FormCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label_Mail = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label_CNAME = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonChangeElement = new System.Windows.Forms.Button();
+            this.buttonDeleteElement = new System.Windows.Forms.Button();
+            this.buttonAddElement = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(656, 535);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(132, 32);
+            this.buttonCancel.TabIndex = 11;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(500, 535);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(132, 32);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(469, 555);
+            this.dataGridView.TabIndex = 12;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(500, 105);
+            this.textBoxPrice.Multiline = true;
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(274, 27);
+            this.textBoxPrice.TabIndex = 16;
+            // 
+            // label_Mail
+            // 
+            this.label_Mail.AutoSize = true;
+            this.label_Mail.Location = new System.Drawing.Point(497, 79);
+            this.label_Mail.Name = "label_Mail";
+            this.label_Mail.Size = new System.Drawing.Size(33, 13);
+            this.label_Mail.TabIndex = 15;
+            this.label_Mail.Text = "Цена";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(500, 37);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(274, 27);
+            this.textBoxName.TabIndex = 14;
+            // 
+            // label_CNAME
+            // 
+            this.label_CNAME.AutoSize = true;
+            this.label_CNAME.Location = new System.Drawing.Point(497, 12);
+            this.label_CNAME.Name = "label_CNAME";
+            this.label_CNAME.Size = new System.Drawing.Size(57, 13);
+            this.label_CNAME.TabIndex = 13;
+            this.label_CNAME.Text = "Название";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Детали :";
+            // 
+            // buttonChangeElement
+            // 
+            this.buttonChangeElement.Location = new System.Drawing.Point(500, 350);
+            this.buttonChangeElement.Name = "buttonChangeElement";
+            this.buttonChangeElement.Size = new System.Drawing.Size(184, 41);
+            this.buttonChangeElement.TabIndex = 20;
+            this.buttonChangeElement.Text = "Изменить";
+            this.buttonChangeElement.UseVisualStyleBackColor = true;
+            this.buttonChangeElement.Click += new System.EventHandler(this.buttonChangeElement_Click);
+            // 
+            // buttonDeleteElement
+            // 
+            this.buttonDeleteElement.Location = new System.Drawing.Point(500, 303);
+            this.buttonDeleteElement.Name = "buttonDeleteElement";
+            this.buttonDeleteElement.Size = new System.Drawing.Size(184, 41);
+            this.buttonDeleteElement.TabIndex = 19;
+            this.buttonDeleteElement.Text = "Удалить";
+            this.buttonDeleteElement.UseVisualStyleBackColor = true;
+            this.buttonDeleteElement.Click += new System.EventHandler(this.buttonDeleteElement_Click);
+            // 
+            // buttonAddElement
+            // 
+            this.buttonAddElement.Location = new System.Drawing.Point(500, 256);
+            this.buttonAddElement.Name = "buttonAddElement";
+            this.buttonAddElement.Size = new System.Drawing.Size(184, 41);
+            this.buttonAddElement.TabIndex = 18;
+            this.buttonAddElement.Text = "Добавить";
+            this.buttonAddElement.UseVisualStyleBackColor = true;
+            this.buttonAddElement.Click += new System.EventHandler(this.buttonAddElement_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(500, 397);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(184, 41);
+            this.buttonUpdate.TabIndex = 21;
+            this.buttonUpdate.Text = "Обновить";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(500, 177);
+            this.textBoxYear.Multiline = true;
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(274, 27);
+            this.textBoxYear.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Год выпуска";
+            // 
+            // FormCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 579);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonChangeElement);
+            this.Controls.Add(this.buttonDeleteElement);
+            this.Controls.Add(this.buttonAddElement);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.label_Mail);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label_CNAME);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Name = "FormCar";
+            this.Text = " Добавить автомобиль";
+            this.Load += new System.EventHandler(this.FormCar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label_Mail;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label_CNAME;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonChangeElement;
+        private System.Windows.Forms.Button buttonDeleteElement;
+        private System.Windows.Forms.Button buttonAddElement;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Label label2;
+    }
+}
